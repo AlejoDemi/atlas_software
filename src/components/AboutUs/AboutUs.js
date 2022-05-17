9ec84fabe963d991a8704ef86f4a7d6b096fe4cd
alejoDemi <alejo.demitropulos@ing.austral.edu.ar>
2022-05-17 18:19:24 -0300

    start

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -33,6 +33,7 @@ const AboutUs = () => {
             <h1 className={"tittle"}>ABOUT US</h1>
             <div className={"text"}>
                 <p>We are a tailor-made software company with the drive and skills necessary to take your business to the next level. </p>
+                <p>Born and founded by excellence-driven Software Engineering students, with the philosophy of achieving top-tier quality in all aspects.</p>
                 <p>We seek to fully unleash the power of new, innovative technologies, along with the stability provided by fundamental classics. </p>
                 <p>Our goal is to cater for the needs of our clients, for we believe that together, we will make the world move forward. </p>
             </div>
--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,73 +1,41 @@
 import "./ContactUs.css"
 
-//const ContactUs = () => {
-//
-//    function sendEmail(e) {
-//        e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
-//
-//        emailjs.sendForm('service_gbt78fb', '__ejs-test-mail-service__', e.target, 'ZXtGxlcLxQ26uoMyZvvFk')
-//            .then((result) => {
-//                window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior)
-//            }, (error) => {
-//                console.log(error.text);
-//            });
-//    }
-//
-//    return (
-//        <div className={"contactUsBox"} style={{zIndex:"3"}} >
-//            <h1 className={"tittle"}>CONTACT US</h1>
-//            <div className={"form"} onSubmit={()=>sendEmail}>
-//                <input className={"input"} type={"text"} placeholder={"Full Name"}/>
-//                <input className={"input"} type={"email"} placeholder={"Email"}/>
-//                <textarea className={"input"} type={"text"} style={{height:"80px"}} placeholder={"Message"}/>
-//                <input type={"submit"} value={"Send"} className={"button"} />
-//            </div>
-//        </div>
-//    );
-//};
-//
-//
-import React, { useRef } from 'react';
+import React, {useRef, useState} from 'react';
 import emailjs from '@emailjs/browser';
 
 const ContactUs = () => {
-  const form = useRef();
+
+    const [fullName, setFullName ] = useState("");
+    const [email, setEmail ] = useState("");
+    const [message, setMessage ] = useState("");
+
+
+    const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
+    if (fullName!=="" && email!=="" && message!==""){
+        emailjs.sendForm('service_y521b0g', 'template_20sozcf', form.current, 'wX92O6_gcyV1wAX7j')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
+    else console.log("error bolu")
 
-    emailjs.sendForm('service_y521b0g', 'template_20sozcf', form.current, 'wX92O6_gcyV1wAX7j')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
   };
 
   return (
     <div className={"contactUsBox"} style={{zIndex:"3"}} >
         <h1 className={"tittle"}>CONTACT US</h1>
-        <div className={"form"} >
-        <form ref={form} onSubmit={sendEmail}>
-            <input name = "user_name" className={"input"} type="text" placeholder={"Full Name"}/>
-            <input name = "user_email" className={"input"} type="email" placeholder={"Email"}/>
-            <textarea name = "message" className={"input"} type="text" style={{height:"80px"}} placeholder={"Message"}/>
+        <form ref={form} onSubmit={sendEmail} className={"form"}>
+            <input name = "user_name" className={"input"} type="text" placeholder={"Full Name"} onChange={e=>setFullName(e.target.value)}/>
+            <input name = "user_email" className={"input"} type="email" placeholder={"Email"} onChange={e=>setEmail(e.target.value)}/>
+            <textarea name = "message" className={"input"} type="text" style={{height:"80px"}} placeholder={"Message"} onChange={e=>setMessage(e.target.value)}/>
             <input type="submit" value="Send" className={"button"} />
         </form>
-        </div>
     </div>
 );
-
-  //return (
-  //  <form ref={form} onSubmit={sendEmail}>
-  //    <label>Name</label>
-  //    <input type="text" name="user_name" />
-  //    <label>Email</label>
-  //    <input type="email" name="user_email" />
-  //    <label>Message</label>
-  //    <textarea name="message" />
-  //    <input type="submit" value="Send" />
-  //  </form>
-  //);
 };
 export default ContactUs;
