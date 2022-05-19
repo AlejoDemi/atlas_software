a0b557ce985e080f5ca715763000179b5995836e
AlejoDemi <alejo.demitropulos@ing.austral.edu.ar>
2022-05-19 00:35:27 -0300

    Mobile Implementation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Canvas from "./components/Canvas/Canvas";
 import {useEffect, useState} from "react";
 import { SpinnerDotted } from 'spinners-react';
+import CanvasMobile from "./components/MobileVersion/Canvas/CanvasMobile";
 
 function App() {
 
@@ -23,12 +24,17 @@ function App() {
     });
 
   return (
-      timer?
-          <div style={{height:"100vh",width:"100vw",display:"flex",justifyContent:"center",alignContent:"center",background:"#001027"}}>
-            <SpinnerDotted size={50} thickness={180} speed={99} color="#f3c9d0" />
-          </div>
-            :
-            <Canvas/>
+      isDesktop?
+
+              timer?
+                  <div style={{height:"100vh",width:"100vw",display:"flex",justifyContent:"center",alignContent:"center",background:"#001027"}}>
+                      <SpinnerDotted size={50} thickness={180} speed={99} color="#f3c9d0" />
+                  </div>
+                  :
+                  <Canvas/>
+
+          :
+          <CanvasMobile></CanvasMobile>
   );
 }
 
---
+++ src/components/MobileVersion/Canvas/CanvasMobile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CanvasMobile = () => {
+    return (
+        <div style={{background:"#001027"}}>
+            <h1 style={{color:"white"}}>Under Development</h1>
+        </div>
+    );
+};
+
+export default CanvasMobile;
