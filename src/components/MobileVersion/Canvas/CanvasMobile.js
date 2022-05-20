3abeaa9d03198d23ba76951fdbe62cd5e7727baa
AlejoDemi <alejo.demitropulos@ing.austral.edu.ar>
2022-05-20 01:19:44 -0300

    Mobile Implementation

--- src/components/MobileVersion/Canvas/CanvasMobile.js
+++ src/components/MobileVersion/Canvas/CanvasMobile.js
@@ -3,7 +3,7 @@ import PageBinder from "../../Binder/PageBinder";
 
 const CanvasMobile = () => {
     return (
-        <div style={{background:"#001027",height:"300vh",display:"flex",justifyContent:"center",textAlign:"center"}}>
+        <div style={{background:"#001027",height:"350vh",display:"flex",justifyContent:"center",textAlign:"center"}}>
             <PageBinder></PageBinder>
         </div>
     );
