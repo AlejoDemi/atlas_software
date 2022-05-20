9d7be8aab7e8af6cbbaa0399fc97ccf2b736042e
alejoDemi <alejo.demitropulos@ing.austral.edu.ar>
2022-05-20 15:07:14 -0300

    start

--- src/components/MobileVersion/Canvas/CanvasMobile.js
+++ src/components/MobileVersion/Canvas/CanvasMobile.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import PageBinder from "../../Binder/PageBinder";
+import AboutUs from "../../AboutUs/AboutUs";
+import ContactUs from "../../ContactUs/ContactUs";
+import "./CanvasMobile.css"
 
 const CanvasMobile = () => {
+
     return (
-        <div style={{background:"#001027",height:"350vh",display:"flex",justifyContent:"center",textAlign:"center"}}>
-            <PageBinder></PageBinder>
+        <div style={{width:"100%",background:"lightgray",height:"410vh",display:"flex",justifyContent:"center",textAlign:"center"}}>
+            <div >
+                <div className={"headerMobile"} style={{height:"100vh"}}>
+
+                </div>
+                <div style={{height:"210vh"}}>
+                    <AboutUs></AboutUs>
+                </div>
+                <div style={{height:"100vh",display:"flex",justifyContent:"center"}}>
+                    <ContactUs></ContactUs>
+
+                </div>
+
+            </div>
         </div>
     );
 };
