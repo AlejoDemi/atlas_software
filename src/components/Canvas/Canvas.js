65859df9f5e1f4b849b7a2f9146e6677aaec9783
Pedro Elizalde <pedro.elizalde@ing.austral.edu.ar>
2022-05-20 00:54:30 -0300

    minor changes

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -59,7 +59,7 @@ const Canvas = () => {
                 <h1 className={"slogan"}>MOVING THE WORLD<div style={{"color": "#E392BE", "fontStyle":"italic"}}> FORWARD.</div></h1>
             </ParallaxLayer>
 
-            <ParallaxLayer sticky={{start:0,end:3}} >
+            <ParallaxLayer sticky={{start:0,end:2.9}} >
                 <Header goToContact={()=>ref.current.scrollTo(4)}
                         goToAboutUs={()=>ref.current.scrollTo(1.9)}
                         goToTop={()=>ref.current.scrollTo(0, {behavior:'auto'})}/>
