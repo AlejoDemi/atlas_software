e4cd6c6fce43723d70f1390efe90ef415dcc2acc
Pedro Elizalde <pedro.elizalde@ing.austral.edu.ar>
2022-05-19 21:15:11 -0300

    new mountains

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -32,21 +32,21 @@ const Canvas = () => {
                 <ParticlesStars style={{height:"10px",width:"10px", position: "absolute"}} interact={isDesktop}/>
             </ParallaxLayer>
 
-            <ParallaxLayer speed={-0.35} offset={0.07} style={{backgroundImage: 'url("https://i.ibb.co/0D0MGTz/light-Logo.png")', backgroundPosition: 'center 5%', repeat:"true", backgroundSize:"400px 410px"}}>
-            </ParallaxLayer>
-            <ParallaxLayer speed={-0.35} offset={0.35} style={{backgroundImage: "url('https://svgshare.com/i/hYk.svg')", backgroundPosition: 'center 30%', repeat:"true"}}>
-            </ParallaxLayer>
+            <ParallaxLayer speed={-0.35} offset={0.1} style={{backgroundImage: "url('https://svgur.com/i/hXz.svg')", backgroundPosition: 'center 90%', repeat:"true"}}>
+            </ParallaxLayer>            
             <ParallaxLayer speed={-0.25} offset={0.25} style={{backgroundImage:'url("https://svgur.com/i/hYm.svg")', backgroundPosition: 'center 70%', repeat:"true"}}>
             </ParallaxLayer>
-            <ParallaxLayer speed={-0.2} offset={0.25} style={{backgroundImage:'url("https://svgur.com/i/hYD.svg")', backgroundPosition: 'center 40%', repeat:"true"}}>
+            <ParallaxLayer speed={-0.2} offset={0.25} style={{backgroundImage:'url("https://svgur.com/i/hZK.svg")', backgroundPosition: 'center 40%', repeat:"true"}}>
             </ParallaxLayer>
             <ParallaxLayer speed={0.1} offset={0.25} style={{backgroundImage:'url("https://svgur.com/i/hWu.svg")', backgroundPosition: 'center 50%', repeat:"true"}}>
             </ParallaxLayer>
-            <ParallaxLayer speed={0.1} offset={1} style={{background:"#000723"}}>
+            <ParallaxLayer speed={0.15} offset={0.25} style={{backgroundImage:'url("https://svgur.com/i/hZA.svg")', backgroundPosition: 'center 70%', repeat:"true"}}>
             </ParallaxLayer>
-            <ParallaxLayer speed={0.15} offset={0.25} style={{backgroundImage:'url("https://svgur.com/i/hYa.svg")', backgroundPosition: 'center 70%', repeat:"true"}}>
+            <ParallaxLayer speed={0.5} offset={0.6} style={{backgroundImage:'url("https://svgur.com/i/hYU.svg")', backgroundPosition:"center 0%", repeat:"true"}}>
             </ParallaxLayer>
-            <ParallaxLayer speed={0.35} offset={0.65} style={{backgroundImage:'url("https://svgur.com/i/hXM.svg")', backgroundPosition:"center 0%", repeat:"true"}}>
+
+            <ParallaxLayer speed={0.5} offset={0.9} style={{background:"#000723"}}>
+                <div style={{background:"#000723", backgroundSize:"1000px 1000px"}}></div>
             </ParallaxLayer>
 
             <ParallaxLayer speed={0.3} offset={0.9}>
